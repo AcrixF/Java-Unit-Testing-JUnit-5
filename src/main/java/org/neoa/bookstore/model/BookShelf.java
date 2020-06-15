bd371eab1c184ad07c10ceb9ec428eebbe720f8a
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-06-15 11:29:16 -0500

    AssertJ Expresive assertions

--- src/main/java/org/neoa/bookstore/model/BookShelf.java
+++ src/main/java/org/neoa/bookstore/model/BookShelf.java
@@ -1,10 +1,13 @@
 package org.neoa.bookstore.model;
 
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class BookShelf {
@@ -27,6 +30,15 @@ public class BookShelf {
         return books.stream()
                 .sorted(criteria)
                 .collect(Collectors.toList());
+    }
+
+    public Map<Year, List<Book>> groupByPublicationYear() {
+       return groupBy(book -> Year.of(book.getPublishedOn().getYear()));
+    }
+
+    public <K> Map<K, List<Book>> groupBy(Function<Book, K> groupByFunction) {
+        return this.books.stream()
+                .collect(Collectors.groupingBy(groupByFunction));
     }
 
 }
--- src/test/java/org/neoa/bookstore/BookShelfSpec.java
+++ src/test/java/org/neoa/bookstore/BookShelfSpec.java
@@ -1,7 +1,6 @@
 package org.neoa.bookstore;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
@@ -10,11 +9,16 @@ import org.neoa.bookstore.model.BookShelf;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.Year;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("<= BookShelf Specification =>")
 public class BookShelfSpec {
@@ -24,6 +28,7 @@ public class BookShelfSpec {
     private Book effectiveJava;
     private Book codeComplete;
     private Book mythicalManMonth;
+    private Book cleanCode;
 
     @BeforeEach
     public void init() throws Exception {
@@ -31,6 +36,7 @@ public class BookShelfSpec {
         effectiveJava = new Book("Effective Java", "Joshua Bloch", LocalDate.of(2008, Month.MAY, 8));
         codeComplete = new Book("Code Complete", "Steve McConnel", LocalDate.of(2004, Month.JUNE, 9));
         mythicalManMonth = new Book("The Mythical Man-Month", "Frederick Phillips Brooks", LocalDate.of(1975, Month.JANUARY, 1));
+        cleanCode = new Book("Clean Code", "Robert C. Martin", LocalDate.of(2008, Month.JANUARY, 10));
     }
 
     @Test
@@ -88,13 +94,54 @@ public class BookShelfSpec {
 
     @Test
     @DisplayName("Bookshelf Arranged By User Provided Criteria")
-    @Disabled("Needs to implement Comparator")
     public void bookshelfArrangedByUserProvidedCriteria() {
         shelf.add(effectiveJava, codeComplete, mythicalManMonth);
-        List<Book> books = shelf.arrange(Comparator.<Book>naturalOrder().reversed());
-        assertEquals(Arrays.asList(mythicalManMonth, effectiveJava, codeComplete), books, () -> "Books in a bookshelf are arranged in descending order of book title");
+        Comparator<Book> reversed = Comparator.<Book> naturalOrder().reversed();
+        List<Book> books = shelf.arrange(reversed);
+        assertThat(books).isSortedAccordingTo(reversed);
+    }
+
+    @Test
+    @DisplayName("Group Books Inside BookShelf By Publication Year")
+    public void groupBooksInsideBookShelfByPublicationYear() {
+        shelf.add(effectiveJava, codeComplete, mythicalManMonth, cleanCode);
+        Map<Year, List<Book>> booksByPublicationYear = shelf.groupByPublicationYear();
+
+        assertThat(booksByPublicationYear)
+                .containsKey(Year.of(2008))
+                .containsValues(Arrays.asList(effectiveJava, cleanCode));
+
+        assertThat(booksByPublicationYear)
+                .containsKey(Year.of(2004))
+                .containsValues(Collections.singletonList(codeComplete));
+
+        assertThat(booksByPublicationYear)
+                .containsKey(Year.of(1975))
+                .containsValues(Collections.singletonList(mythicalManMonth));
     }
 
 
+    @Test
+    @DisplayName("Books inside bookshelf are grouped according to user provided criteria(group by author name)")
+    public void groupBooksByUserProvidedCriteria() {
+        shelf.add(effectiveJava, codeComplete, mythicalManMonth, cleanCode);
+        Map<String, List<Book>> booksByAuthor = shelf.groupBy(Book::getAuthor);
+
+        assertThat(booksByAuthor)
+                .containsKey("Joshua Bloch")
+                .containsValues(Collections.singletonList(effectiveJava));
+
+        assertThat(booksByAuthor)
+                .containsKey("Steve McConnel")
+                .containsValues(Collections.singletonList(codeComplete));
+
+        assertThat(booksByAuthor)
+                .containsKey("Frederick Phillips Brooks")
+                .containsValues(Collections.singletonList(mythicalManMonth));
+
+        assertThat(booksByAuthor)
+                .containsKey("Robert C. Martin")
+                .containsValues(Collections.singletonList(cleanCode));
+    }
 
 }
