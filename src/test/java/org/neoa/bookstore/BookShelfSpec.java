29274a36f30d6e66cb1ada32e60d72c9d2da64e7
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-06-15 17:16:01 -0500

    Cached Parameter Resolver

--- src/test/java/org/neoa/bookstore/BookShelfSpec.java
+++ src/test/java/org/neoa/bookstore/BookShelfSpec.java
@@ -178,6 +178,22 @@ public class BookShelfSpec {
     class BookShelfProgress {
 
         @Test
+        @DisplayName("is 40% completed, and 60% to read, when two books read and 3 books no read yet")
+        public void progressWithCompletedProgressAndToReadPercentages() {
+            effectiveJava.startedReadingOn(LocalDate.of(2016, Month.JULY, 1));
+            effectiveJava.finishedReadingOn(LocalDate.of(2016, Month.JULY, 31));
+
+            cleanCode.startedReadingOn(LocalDate.of(2016, Month.AUGUST, 1));
+            cleanCode.finishedReadingOn(LocalDate.of(2016, Month.AUGUST, 31));
+
+            shelf.add(effectiveJava, codeComplete, mythicalManMonth, cleanCode, refactoring);
+
+            Progress progress = shelf.progress();
+            assertThat(progress.completed()).isEqualTo(40);
+            assertThat(progress.toRead()).isEqualTo(60);
+        }
+
+        @Test
         @DisplayName("is 0% completed and 100% to-read when no book is read yet")
         public void progress100PercentUnread() {
             Progress progress = shelf.progress();
@@ -205,21 +221,6 @@ public class BookShelfSpec {
             assertThat(progress.toRead()).isEqualTo(40);
         }
 
-        @Test
-        @DisplayName("is 40% completed, and 60% to read, when two books read and 3 books no read yet")
-        public void progressWithCompletedProgressAndToReadPercentages() {
-            effectiveJava.startedReadingOn(LocalDate.of(2016, Month.JULY, 1));
-            effectiveJava.finishedReadingOn(LocalDate.of(2016, Month.JULY, 31));
-
-            cleanCode.startedReadingOn(LocalDate.of(2016, Month.AUGUST, 1));
-            cleanCode.finishedReadingOn(LocalDate.of(2016, Month.AUGUST, 31));
-
-            shelf.add(effectiveJava, codeComplete, mythicalManMonth, cleanCode, refactoring);
-
-            Progress progress = shelf.progress();
-            assertThat(progress.completed()).isEqualTo(40);
-            assertThat(progress.toRead()).isEqualTo(60);
-        }
     }
 
 }
--- src/test/java/org/neoa/bookstore/BooksParameterResolver.java
+++ src/test/java/org/neoa/bookstore/BooksParameterResolver.java
@@ -15,6 +15,18 @@ import java.util.Objects;
 
 public class BooksParameterResolver implements ParameterResolver {
 
+    private final Map<String, Book> books;
+
+    public BooksParameterResolver() {
+        Map<String, Book> books = new HashMap<>();
+        books.put("Effective Java", new Book("Effective Java", "Joshua Bloch", LocalDate.of(2008, Month.MAY, 8)));
+        books.put("Code Complete", new Book("Code Complete", "Steve McConnel", LocalDate.of(2004, Month.JUNE, 9)));
+        books.put("The Mythical Man-Month", new Book("The Mythical Man- Month", "Frederick Phillips Brooks", LocalDate.of(1975, Month.JANUARY, 1)));
+        books.put("Clean Code", new Book("Clean Code", "Robert C. Martin", LocalDate.of(2008, Month.AUGUST, 1)));
+        books.put("Refactoring: Improving the Design of Existing Code", new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", LocalDate.of(2002, Month.MARCH, 9)));
+        this.books = books;
+    }
+
     @Override
     public boolean supportsParameter(ParameterContext parameterContext,
                                      ExtensionContext extensionContext) throws ParameterResolutionException {
@@ -26,12 +38,6 @@ public class BooksParameterResolver implements ParameterResolver {
     @Override
     public Object resolveParameter(ParameterContext parameterContext,
                                    ExtensionContext extensionContext) throws ParameterResolutionException {
-        Map<String, Book> books = new HashMap<>();
-        books.put("Effective Java", new Book("Effective Java", "Joshua Bloch", LocalDate.of(2008, Month.MAY, 8)));
-        books.put("Code Complete", new Book("Code Complete", "Steve McConnel", LocalDate.of(2004, Month.JUNE, 9)));
-        books.put("The Mythical Man-Month", new Book("The Mythical Man- Month", "Frederick Phillips Brooks", LocalDate.of(1975, Month.JANUARY, 1)));
-        books.put("Clean Code", new Book("Clean Code", "Robert C. Martin", LocalDate.of(2008, Month.AUGUST, 1)));
-        books.put("Refactoring: Improving the Design of Existing Code", new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", LocalDate.of(2002, Month.MARCH, 9)));
         return books;
     }
 }
